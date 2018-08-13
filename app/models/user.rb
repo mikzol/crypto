a826d3ce199dcade7f2f6a9af7d737e3b77047b6
mikzol <magicmikevv@gmail.com>
2018-08-13 19:00:56 +0100

    added associations and fixed crypto table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :cryptocurrencies
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_174628) do
+ActiveRecord::Schema.define(version: 2018_08_13_175903) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_174628) do
 
   create_table "cryptocurrencies", force: :cascade do |t|
     t.string "name"
+    t.string "symbol"
   end
 
   create_table "users", force: :cascade do |t|
