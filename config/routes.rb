80bb12209ed5eb05ca1145e7467a8f92d457d26b
mikzol <magicmikevv@gmail.com>
2018-08-14 11:58:10 +0100

    added user permissions for admin & api route for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  # TODO: check if this needs to be scoped to /api later
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
     resources :cryptocurrencies
+  end
+  scope '/api' do
+    devise_for :users
   end
 end
