a5a5a1561ff08c68d461f5caa7646b97e3cb1724
mikzol <magicmikevv@gmail.com>
2018-08-14 12:41:20 +0100

    commiting so I can eject

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.fetch('/api/cryptocurrencies/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Button type="primary">Button</Button>
       </div>
     );
   }
