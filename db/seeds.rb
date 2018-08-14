4a3df66242f01a898456f361ec99dab0c042208f
mikzol <magicmikevv@gmail.com>
2018-08-14 10:27:09 +0100

    added a seed function that creates the top 100 cryptos and puts into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+# TODO: create some way to put all cryptos here
+
+require 'net/http'
+require 'json'
+url = 'https://api.coinmarketcap.com/v2/ticker/'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+r = JSON.parse(response)
+
+data = r['data']
+
+#  for each item in data we want to loop over it and get the name and symbol
+data.each do |item|
+  Cryptocurrency.create!(
+    name: item[1]['name'],
+    symbol: item[1]['symbol']
+  )
+end
