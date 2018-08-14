1e93b1a2875c61c1eb3081e261536b42d383e6c1
mikzol <magicmikevv@gmail.com>
2018-08-14 17:19:21 +0100

    cleaned react files and added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import logo from './logo.svg';
-import './app.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Components/Layout/Home/Home';
 
-@inject('authStore')
-@observer
 class App extends Component {
-  componentDidMount() {
-    window
-      .fetch('/api/cryptocurrencies/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error));
-    console.log(this.props.authStore.isLoaded);
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
