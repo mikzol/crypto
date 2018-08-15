3682a0916a0e352c788243c61abba19e4d657b25
mikzol <magicmikevv@gmail.com>
2018-08-15 17:29:01 +0100

    nevermind that breaks everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 end
