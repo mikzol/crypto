7a0028040d661a61a19aae3afe497a4a7375fa5b
mikzol <magicmikevv@gmail.com>
2018-08-15 17:24:00 +0100

    removed cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :trackable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 end
