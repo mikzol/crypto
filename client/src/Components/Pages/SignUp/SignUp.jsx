de1f7edd14a03f7b4c6aebea0f79e1d26b2a24a2
mikzol <magicmikevv@gmail.com>
2018-08-16 13:22:11 +0100

    added some basic styling for signup/login toggle

--- client/src/Components/Pages/SignUp/SignUp.jsx
+++ client/src/Components/Pages/SignUp/SignUp.jsx
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 export default class SignUp extends Component {
   render() {
     return (
-      <section className="hero">
+      <section className="hero signup">
         <div className="hero-body">
           <div className="container has-text-centered">
             <div className="column is-4 is-offset-4">
               <h3 className="title has-text-grey">Sign Up</h3>
               <div className="box">
-                <div className="row">
-                  <div className="button">Sign Up</div>
-                  <div className="button">Log in</div>
+                <div className="columns is-mobile signup-buttons">
+                  <div className="column is-half-mobile signup-buttons-signup">
+                    <div className="button is-fullwidth">Sign Up</div>
+                  </div>
+                  <div className="column is-half-mobile signup-buttons-login">
+                    <div className="button is-light is-fullwidth">Log in</div>
+                  </div>
                 </div>
                 <form>
                   <div className="field">
