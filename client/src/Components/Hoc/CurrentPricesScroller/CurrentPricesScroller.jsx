3df8e8eb4a1fac142b162343e4a0560dc063aa37
mikzol <magicmikevv@gmail.com>
2018-08-19 18:26:21 +0100

    got basic text loop working

--- client/src/Components/Hoc/CurrentPricesScroller/CurrentPricesScroller.jsx
+++ client/src/Components/Hoc/CurrentPricesScroller/CurrentPricesScroller.jsx
@@ -3,7 +3,11 @@ import axios from 'axios';
 
 export default class CurrentPricesScroller extends Component {
   state = {
-    prices: {}
+    prices: {},
+    // eslint-disable-next-line
+    currentCoin: '',
+    // eslint-disable-next-line
+    currentPrice: ''
   };
 
   // fades up from the bottom
@@ -12,17 +16,40 @@ export default class CurrentPricesScroller extends Component {
   // fades up to the top, repeat
   componentDidMount() {
     axios.get('https://api.coinmarketcap.com/v2/ticker/?limit=5').then(res => {
-      const prices = res.data.data;
+      // eslint-disable-next-line
+      const prices = Object.values(res.data.data);
       this.setState({
         prices
       });
+
       console.log(this.state);
+      // eslint-disable-next-line
+      const len = (this.state.prices).length;
+      console.log(len);
+      console.log(this.state.prices[0]);
+
+      for (let i = 0; i < len; i += 1) {
+        setTimeout(() => {
+          this.setState({
+            currentPrice: prices[i].name,
+            currentCoin: prices[i].quotes.USD.price
+          });
+        }, i * 5000);
+      }
     });
+    // every 3 seconds replace the currentCoin with 1 of the prices, going through 1 by 1
+    //  go back to the first one if the current one is the last one.
+    // const size = Object.keys(this.state.prices).length;
+    // setInterval(() => {
+    // console.log('fuck');
+    // }, 3000);
   }
   render() {
     return (
       <div className="currentprices">
-        <h1 className="title"> HELLO!!</h1>
+        <h1 className="title">
+          {this.state.currentCoin}: {this.state.currentPrice}
+        </h1>
       </div>
     );
   }
