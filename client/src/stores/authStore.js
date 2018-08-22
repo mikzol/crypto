837b916bbe64c2dcbf2e93a0a9a5cf31286a123d
mikzol <magicmikevv@gmail.com>
2018-08-22 17:05:20 +0100

    refactored to make everything a lot smoother

--- client/src/stores/authStore.js
+++ client/src/stores/authStore.js
@@ -35,54 +35,55 @@ class AuthStore {
   getUserCryptocurrencies = () => {
     this.cryptocurrenciesLoading = true;
     axios.get('/api/user_cryptocurrencies').then(res => {
-      const { cryptocurrencies } = res.data;
-      this.cryptocurrencies = cryptocurrencies;
-      this.getUserCryptoInfo();
+      this.getUserCryptoInfo(res.data.cryptocurrencies);
     });
   };
 
   @action
   addUserCryptocurrencies = coins => {
     this.cryptocurrenciesLoading = true;
-    axios.post('/api/user_cryptocurrencies', coins).then(res => {
-      this.cryptocurrencies = res.data.cryptocurrencies;
-      this.cryptocurrenciesLoading = false;
-      // TODO: fix this because I know it can be done better
-      //  it kind of works but need some way to make it impossible to add one if it already exists
-      if (res.data.cryptocurrencies.length !== this.cryptocurrencies.length) {
-        this.addSingleCryptocurrencies(
-          res.data.cryptocurrencies.slice(res.data.cryptocurrencies.length - coins.coins.length)
-        );
-      }
-    });
+    axios
+      .post('/api/user_cryptocurrencies', coins)
+      .then(res => {
+        // sends all user cryptocurrencies to the function that will find info on each one
+        this.getUserCryptoInfo(res.data.cryptocurrencies);
+      })
+      .catch(err => {
+        this.cryptocurrenciesLoading = false;
+      });
   };
 
-  @action
-  addSingleCryptocurrencies = coins => {
-    coins.map(e =>
-      fetch(`https://api.coinmarketcap.com/v2/ticker/${e.api_id}/`)
-        .then(item => item.json())
-        .then(json => {
-          this.userCryptocurrencies.push(json.data);
-          this.cryptocurrenciesLoading = false;
-        })
-    );
-  };
+  // @action
+  // addSingleCryptocurrencies = coins => {
+  //   this.userCryptocurrencies = [];
+  //   coins.map(e =>
+  //     fetch(`https://api.coinmarketcap.com/v2/ticker/${e.api_id}/`)
+  //       .then(item => item.json())
+  //       .then(json => {
+  //         this.userCryptocurrencies.push(json.data);
+  //       })
+  //   );
+  // };
 
   @observable
   userCryptocurrencies = [];
 
   @action
-  getUserCryptoInfo = () => {
+  getUserCryptoInfo = coins => {
+    this.cryptocurrenciesLoading = true;
     this.userCryptocurrencies = [];
-    this.cryptocurrencies.map(e =>
-      fetch(`https://api.coinmarketcap.com/v2/ticker/${e.api_id}/`)
+    const finished = coins.map(coin =>
+      fetch(`https://api.coinmarketcap.com/v2/ticker/${coin.api_id}/`)
         .then(item => item.json())
-        .then(json => {
-          this.userCryptocurrencies.push(json.data);
+        .then(json => this.userCryptocurrencies.push(json.data))
+        .catch(err => {
           this.cryptocurrenciesLoading = false;
         })
     );
+    console.log(finished);
+    Promise.all(finished).then(() => {
+      this.cryptocurrenciesLoading = false;
+    });
   };
   // TODO: test if this works properly
   @action
