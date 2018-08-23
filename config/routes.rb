9fb70d44f9a8b02e5e7b4312d618a0b0df4e2869
mikzol <magicmikevv@gmail.com>
2018-08-23 17:44:35 +0100

    fixed routing for heroku (almost)

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,15 @@
 
 # config/routes.rb
 Rails.application.routes.draw do
-  # register route
+  # register routes
   post "auth/register", to: "users#register"
   post "auth/login", to: "users#login"
   post "auth/current_user", to: "users#current_user"
-  # get "auth/user_cryptocurrencies", to: "users#user_cryptocurrencies"
-  # post "auth/user_cryptocurrencies", to: "users#add_user_cryptocurrencies"
-  # delete "auth/user_cryptocurrencies", to: "users#remove_user_cryptocurrencies"
-
 
   scope "/api" do
     resources :cryptocurrencies
-  end
-  scope "/api" do
     resources :user_cryptocurrencies
   end
-
-  get "*path", to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  root to: redirect("index.html")
+  get "*other", to: redirect("/")
 end
